ceea4921dc72c9e9b872d5324b025d750723011d
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-08-20 23:24:45 +0100

    --no-amend

--- config/secret.js
+++ config/secret.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
   DEV_DATABASE_URL: process.env.DEV_DATABASE_URL,
   DATABASE_URL_TEST: process.env.DATABASE_URL,
-  DATABASE_URL: process.env.DATABASE_URL
+  DATABASE_URL: process.env.DATABASE_URL,
+  dialect: process.env.dialect
 };
--- database/config/config.js
+++ database/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   test: {
     url: secret.DATABASE_URL_TEST,
-    dialect: 'postgres'
+    dialect: secret.dialect
   },
   staging: {
     url: secret.DATABASE_URL,
