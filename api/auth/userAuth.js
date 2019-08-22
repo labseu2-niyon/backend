1def854d4f1673dea99c9d8572204fa52539c09e
ruihildt <ruihildt@protonmail.com>
2019-08-22 15:15:37 +0200

    Start adding passport local strategy
    Co-authored-by: Damola Adewunmi <damolasd@gmail.com>
    Co-authored-by: Ikechukwu Eze <iykekings36@gmail.com>

--- api/auth/userAuth.js
+++ api/auth/userAuth.js
@@ -1,5 +1,23 @@
 const router = require('express').Router();
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).json({
+        message: 'Something is not right',
+        user
+      });
+    }
+    req.login(user, { session: false }, err => {
+      if (err) {
+        res.send(err);
+      } // generate a signed json web token with the contents of user object and return it in the response           const token = jwt.sign(user, 'your_jwt_secret');
+      return res.json({ user, token });
+    });
+  })(req, res);
+});
 
 router.get(
   '/login/?provider=facebook',
