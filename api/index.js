ab3d313ef22d7145b05ad9949db1436e41cf7ee3
Damola Adewunmi <damolasd@gmail.com>
2019-08-22 17:02:17 +0100

    Deleted user-router file

--- api/index.js
+++ api/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 // Export routes from here
 const mentorRouter = require('./mentors/mentor.router');
 const authRouter = require('./auth/userAuth');
-const userRouter = require('./user/user.router');
 
 router.use('/mentors', mentorRouter);
 router.use('/auth', authRouter);
-router.use('/user', userRouter);
 
 module.exports = router;
