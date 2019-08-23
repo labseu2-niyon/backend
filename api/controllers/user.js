ee42ae0c64fc2198aefd6753a5753f4ac8d895ec
Damola Adewunmi <damolasd@gmail.com>
2019-08-23 13:55:20 +0100

    Added Create User profile endpoint

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,40 +1,40 @@
 const models = require('../../database/models');
+const response = require('../helpers/response');
 
 module.exports = {
   async getUserByUsername(req, res) {
     try {
       const { userName } = req.query;
       const user = await models.Users.findOne({
-        where: { username: userName },
-        include: [
-          {
-            model: models.Locations,
-            as: 'location'
-          }
-        ]
+        where: { username: userName }
       });
       if (user) {
-        return res.status(200).json({ user });
+        return response.success(res, 201, user);
       }
-      return res.status(404).send('User with that username does not exist');
+      return response.error(res, 404, 'User with that username does not exist');
     } catch (error) {
-      return res.status(500).send(error.message);
+      return response.error(res, 500, error.message);
     }
   },
 
   async getAllUsers(req, res) {
     try {
-      const users = await models.Users.findAll({
-        include: [
-          {
-            model: models.Locations,
-            as: 'location'
-          }
-        ]
-      });
-      return res.status(201).json({ users });
+      const users = await models.Users.findAll();
+      return response.success(res, 201, users);
     } catch (error) {
-      return res.status(500).send(error.message);
+      return response.error(res, 500, error.message);
+    }
+  },
+
+  async createUserProfile(req, res) {
+    try {
+      const user = await models.Users.create(req.body);
+      if (user) {
+        return response.success(res, 201, user);
+      }
+      return response.error(res, 404, 'Could not create Profile');
+    } catch (error) {
+      return response.error(res, 500, error.message);
     }
   }
 };
