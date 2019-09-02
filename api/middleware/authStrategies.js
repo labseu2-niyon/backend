06db77fcfdf7288634a8f39889c3cba7184c6eee
rui hildt <ruihildt@protonmail.com>
2019-09-02 13:09:34 +0200

    Remove unused strategies

--- api/middleware/authStrategies.js
+++ api/middleware/authStrategies.js
@@ -1,9 +1,6 @@
 const passport = require('passport');
-const FaceBookStrategy = require('passport-facebook').Strategy;
 const GitHubStrategy = require('passport-github').Strategy;
-const GoogleStrategy = require('passport-google-oauth').Strategy;
 const LinkedInStrategy = require('passport-linkedin');
-const TwitterStrategy = require('passport-twitter').Strategy;
 const models = require('../../database/models');
 const keys = require('../../config/secret');
 
@@ -52,61 +49,16 @@ function githubStrategy() {
   );
 }
 
-function facebookStrategy() {
-  passport.use(
-    new FaceBookStrategy(
-      {
-        clientID: keys.FACEBOOK_APP_ID,
-        clientSecret: keys.FACEBOOK_APP_SECRET,
-        callbackURL: '/login/?provider=facebook/redirect',
-        profileFields: ['id', 'first-name', 'last-name', 'email-address']
-      },
-      (accessToken, refreshToken, profile, cb) => {
-        return callbackStrategy(profile, cb);
-      }
-    )
-  );
-}
-
-function googleStrategy() {
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.GOOGLE_CLIENT_ID,
-        clientSecret: keys.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'auth/google/redirect'
-      },
-      (accessToken, refreshToken, profile, cb) => {
-        return callbackStrategy(profile, cb);
-      }
-    )
-  );
-}
-
-function linkedinStrategy() {
+function linkedInStrategy() {
   passport.use(
     new LinkedInStrategy(
       {
         consumerKey: keys.LINKEDIN_API_KEY,
         consumerSecret: keys.LINKEDIN_SECRET_KEY,
-        callbackURL: '/login/?provider=linkedIn/redirect'
+        callbackURL: '/api/user/auth/linkedin/callback'
       },
       (accessToken, refreshToken, profile, cb) => {
-        return callbackStrategy(profile, cb);
-      }
-    )
-  );
-}
-
-function twitterStrategy() {
-  passport.use(
-    new TwitterStrategy(
-      {
-        clientID: keys.TWITTER_CONSUMER_KEY,
-        clientSecret: keys.TWITTER_CONSUMER_SECRET,
-        callbackURL: '/login/?provider=twitter/redirect'
-      },
-      (accessToken, refreshToken, profile, cb) => {
+        console.log(profile);
         return callbackStrategy(profile, cb);
       }
     )
@@ -114,9 +66,6 @@ function twitterStrategy() {
 }
 
 module.exports = {
-  facebookStrategy,
   githubStrategy,
-  googleStrategy,
-  linkedinStrategy,
-  twitterStrategy
+  linkedInStrategy
 };
