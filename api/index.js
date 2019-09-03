7ff01eb93aa397b9b62373facc1b5d37b066b5e2
Damola Adewunmi <damolasd@gmail.com>
2019-09-03 11:25:45 +0100

    getMentoringTypes written

--- api/index.js
+++ api/index.js
@@ -5,10 +5,12 @@ const userRouter = require('./routes/userRoute');
 const mentorRouter = require('./routes/mentorRoute');
 const menteeRouter = require('./routes/menteeRoute');
 const jobsRouter = require('./routes/jobsRoute');
+const mentorTypes = require('./routes/mentoringTypesRoute');
 
 router.use('/user', userRouter);
 router.use('/mentor', mentorRouter);
 router.use('/mentee', menteeRouter);
 router.use('/jobs', jobsRouter);
+router.use('/types', mentorTypes);
 
 module.exports = router;
