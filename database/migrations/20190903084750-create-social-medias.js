d86d7329fa53fc866a9da8fdc27d7b52f842242d
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-09-03 14:40:49 +0100

    ft/socialMedia -created social media Table -seeded social media Table -created endpoint to add a user's social media handle

---
+++ database/migrations/20190903084750-create-social-medias.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Social_medias', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'user'
+        }
+      },
+      facebook: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+      },
+      twitter: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+      },
+      linkedin: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Social_medias');
+  }
+};
