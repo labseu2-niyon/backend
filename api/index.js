4c4257010c1c1ea7f8fb6e88a0b66200c96c38db
Damola Adewunmi <damolasd@gmail.com>
2019-09-03 10:04:07 +0100

    getallJobs written and tested

--- api/index.js
+++ api/index.js
@@ -4,9 +4,11 @@ const router = require('express').Router();
 const userRouter = require('./routes/userRoute');
 const mentorRouter = require('./routes/mentorRoute');
 const menteeRouter = require('./routes/menteeRoute');
+const jobsRouter = require('./routes/jobsRoute');
 
 router.use('/user', userRouter);
 router.use('/mentor', mentorRouter);
 router.use('/mentee', menteeRouter);
+router.use('/jobs', jobsRouter);
 
 module.exports = router;
