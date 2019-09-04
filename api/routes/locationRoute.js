62e7331a6e262482cbfa2c94d939cc97fb38b33e
Damola Adewunmi <damolasd@gmail.com>
2019-09-04 21:08:02 +0100

    Removed  auth protection from locatiom route

--- api/routes/locationRoute.js
+++ api/routes/locationRoute.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
 const controller = require('../controllers/location');
-const authUser = require('../helpers/jwt');
 
-router.post(
-  '/getLocation',
-  [authUser.authUser],
-  controller.findOrCreateLocation
-);
+router.post('/getLocation', controller.findOrCreateLocation);
 
 module.exports = router;
