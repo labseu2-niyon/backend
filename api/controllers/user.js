865727b5449666d97118c8b4e222c5cd0513b710
rui hildt <ruihildt@protonmail.com>
2019-09-05 16:52:42 +0200

    Add Mentee and Mentor to getAllUsers

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -95,9 +95,9 @@ module.exports = {
             as: 'location'
           },
           {
-            model: models.Tech_jobs,
-            attributes: ['tech_name'],
-            as: 'job',
+            model: models.Mentees,
+            attributes: ['id'],
+            as: 'Mentee',
             include: [
               {
                 model: models.Industries,
@@ -105,6 +105,23 @@ module.exports = {
                 as: 'industry'
               }
             ]
+          },
+          {
+            model: models.Mentors,
+            attributes: ['id'],
+            as: 'Mentor',
+            include: [
+              {
+                model: models.Industries,
+                attributes: ['industry_name'],
+                as: 'industry'
+              }
+            ]
+          },
+          {
+            model: models.Tech_jobs,
+            attributes: ['tech_name'],
+            as: 'job'
           }
         ]
       });
